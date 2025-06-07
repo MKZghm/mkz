4801032da328c14ad6967582e740a2e1e074d0d6
MKZghm <comunello.henrique@escola.pr.gov.br>
2025-06-07 15:07:47 -0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,5 @@
+fruta1 = (input("digite um nome de uma fruta"))
+fruta2 = (input("digite um nome de uma fruta"))
+fruta2 = (input("digite um nome de uma fruta"))
+
+print("-", fruta2)
---
+++ ex4.py
@@ -0,0 +1,4 @@
+numeros = [1, 2, 3]
+numeros.append(4)
+del numeros[1]
+print(numeros)
